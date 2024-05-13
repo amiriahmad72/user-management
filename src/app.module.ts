bdb96e731ec2acf67e19d523885769db03fca68e
Ahmad Amiri <amiriahmad72@gmail.com>
2024-05-14 00:30:55 +0330

    chore: remove not-user controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { UsersModule } from './users/users.module';
 import { QueueModule } from './queue/queue.module';
@@ -20,7 +18,5 @@ import { QueueModule } from './queue/queue.module';
     EmailModule,
     QueueModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
