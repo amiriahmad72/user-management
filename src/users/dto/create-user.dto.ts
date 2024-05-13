dded23afadb45feb1aab0a78f922b667a3f58525
Ahmad Amiri <amiriahmad72@gmail.com>
2024-05-13 14:01:22 +0330

    feat: add validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, Length, max } from "class-validator";
+
 export class CreateUserDto {
+
+  @Length(1, 100)
+  @IsNotEmpty()
   firstName: string;
+
+  @Length(1, 100)
+  @IsNotEmpty()
   lastName: string;
+
+  @IsEmail()
   email: string;
+
 }
