7225847c25ee8f9dbd2ff1681489dc6de0908044
Ahmad Amiri <amiriahmad72@gmail.com>
2024-05-15 02:25:54 +0330

    test: e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { connections } from 'mongoose';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -15,10 +17,37 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    for (let i in connections) {
+      await connections[i].close();
+    }
+    await app.close();
   });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+    for (let i in connections) {
+      const collections = connections[i].collections;
+      for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany({});
+      }
+    }
+  });
+
+  it('/users (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/users')
+      .expect(200)
+      .expect('[]');
+  });
+
+  it('should return a user, send email and send rabbitMq event', async () => {
+    const createUserDto: CreateUserDto = { firstName: 'dummyFirstName', lastName: 'dummyLastName', email: 'dummyEmail@mail.com' };
+    return request(app.getHttpServer())
+      .post('/users').send(createUserDto)
+      .expect(201)
+      .expect((response) => response.body == createUserDto);
+  });
+
 });
